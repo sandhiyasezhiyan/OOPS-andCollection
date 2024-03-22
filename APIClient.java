5ba56a9998a5a10ed3a0a023d85557f242e04580
sandhiyasezhiyan <84834997+sandhiyasezhiyan@users.noreply.github.com>
2024-03-22 15:31:13 +0530

    Update APIClient.java

--- APIClient.java
+++ APIClient.java
@@ -5,7 +5,7 @@ public void sendrequest(String endpoint) {
 	System.out.println(endpoint);
 }
 
-public void sendrequest1(String endpoint,String requestbody,boolean requeststatus) {
+public void sendrequest(String endpoint,String requestbody,boolean requeststatus) {
 	System.out.println(endpoint);
 	System.out.println(requestbody);
 	System.out.println(true);
@@ -15,6 +15,7 @@ public void sendrequest1(String endpoint,String requestbody,boolean requeststatu
 public static void main(String[] args) {
 	APIClient api = new APIClient();
 	api.sendrequest("endpoint is passed");
-	api.sendrequest1("endpoint is passed", "requestbody is passing", true);
+	api.sendrequest("endpoint is passed", "requestbody is passing", true);
 }
 }
+
